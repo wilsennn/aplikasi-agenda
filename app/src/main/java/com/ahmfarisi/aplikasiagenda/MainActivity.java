97cdafaad8e05b82eb2d64976290692514183217
wilsennn <wilsenfendrius@mhs.mdp.ac.id>
2023-03-10 15:01:40 +0700

    aplikasi agenda

--- app/src/main/java/com/ahmfarisi/aplikasiagenda/MainActivity.java
+++ app/src/main/java/com/ahmfarisi/aplikasiagenda/MainActivity.java
@@ -1,14 +1,31 @@
 package com.ahmfarisi.aplikasiagenda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fabTambah;
+    private RecyclerView rvAgenda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabTambah = findViewById(R.id.fab_tambah);
+        rvAgenda = findViewById(R.id.rv_agenda);
+
+        fabTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TambahActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/ahmfarisi/aplikasiagenda/TambahActivity.java
+++ app/src/main/java/com/ahmfarisi/aplikasiagenda/TambahActivity.java
@@ -3,12 +3,52 @@ package com.ahmfarisi.aplikasiagenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TambahActivity extends AppCompatActivity {
+    private EditText etTanggal, etJam, etKegiatan;
+    private Button btnTambah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
+
+        etTanggal = findViewById(R.id.et_tanggal);
+        etJam = findViewById(R.id.et_jam);
+        etKegiatan = findViewById(R.id.et_kegiatan);
+        btnTambah = findViewById(R.id.btn_tambah);
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tanggal, jam, kegiatan;
+
+                tanggal = etTanggal.getText().toString();
+                jam = etJam.getText().toString();
+                kegiatan = etKegiatan.getText().toString();
+
+                if (tanggal.trim().equals("")) {
+                    etTanggal.setError("Tanggal Harus Diisi");
+                } else if (jam.trim().equals("")) {
+                    etJam.setError("jam Harus Diisi");
+                } else if (kegiatan.trim().equals("")) {
+                    etKegiatan.setError("kegiatan harus diisi");
+                } else {
+                    MyDatabaseHelper myDB = new MyDatabaseHelper(TambahActivity.this);
+                    long eks = myDB.tambahAgenda(tanggal, jam, kegiatan);
+                    if (eks == -1) {
+                        Toast.makeText(TambahActivity.this, "Tambah Data Gagal!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(TambahActivity.this, "Tambah Data Sukses", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+            }
+        });
+
     }
 }
