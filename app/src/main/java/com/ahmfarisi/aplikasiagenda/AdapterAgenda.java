b2cb067ef39870c76c02143b1d67cbf7e5717854
wilsennn <wilsenfendrius@mhs.mdp.ac.id>
2023-03-24 14:10:19 +0700

    again

--- app/src/main/java/com/ahmfarisi/aplikasiagenda/AdapterAgenda.java
+++ app/src/main/java/com/ahmfarisi/aplikasiagenda/AdapterAgenda.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -66,7 +67,17 @@ public class AdapterAgenda extends RecyclerView.Adapter<AdapterAgenda.VHAgenda> 
                     pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-
+                            MyDatabaseHelper myDB = new MyDatabaseHelper(ctx);
+                            long eks = myDB.hapusAgenda(tvId.getText().toString());
+                            if(eks == -1){
+                                Toast.makeText(ctx, "Gagal Menghapus Data", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            }
+                            else {
+                                Toast.makeText(ctx, "Sukses Menghapus Data!", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                                ((MainActivity) ctx).onResume();
+                            }
                         }
                     });
 
